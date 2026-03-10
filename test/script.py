5edbb560a818a6bda55f7e61cf2130ecf21e3113
Sebastian Treanor <ramerize96@gmail.com>
2026-03-10 04:31:32 +0000

    reverting changes

--- test/script.py
+++ test/script.py
@@ -1,13 +1,13 @@
 from pathlib import Path
 
 current_dir = Path.cwd()
-# current_file = Path(__file__).name
+current_file = Path(__file__).name
 
 print(f"Files in {current_dir}:")
 
 for filepath in current_dir.iterdir():
-    #if filepath.name == current_file:
-     #   continue
+    if filepath.name == current_file:
+        continue
 
     print(f"    - {filepath.name}")
 
