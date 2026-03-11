dc8670edca8be2ba19cb0c5469865c0566d3a5b8
Sebastian Treanor <ramerize96@gmail.com>
2026-03-11 02:17:52 +0000

    commit

--- projects/week1/python3.13.11/pipeline.py
+++ projects/week1/python3.13.11/pipeline.py
@@ -6,7 +6,8 @@ print('arguments', sys.argv)
 
 month = int(sys.argv[1])
 
-df = pd.DataFrame()
+df = pd.DataFrame({"day": [1 ,2], "num_passengers": [3, 4]})
+df['month'] = month
 print(df.head())
 
 print(f'hello pipeline, month={month}')
